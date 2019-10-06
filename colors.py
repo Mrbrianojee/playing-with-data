264ae85436d53af477d69be67ef0531df9fe8ea1
Brian O Grady <brian@codeinstitute.net>
2019-10-06 14:02:25 +0000

    refactored to use pandas

--- colors.py
+++ colors.py
@@ -1,5 +1,8 @@
-with open("Colors.txt", 'r') as open_file:
-    for obvservation in open_file:
-        print("Reading data: " + obvservation)
+import pandas as pd
+
+color_table =  pd.io.parsers.read_table("Colors.txt")
+print(color_table)
+
+
 
 
